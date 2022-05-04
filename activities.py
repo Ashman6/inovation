0741ddc53b1b106bb6c3a4f1ed50e07e7a64f328
ash man <ashmankartel6@gmail.com>
2022-05-04 17:17:10 +0100

    python notes

---
+++ activities.py
@@ -0,0 +1,65 @@
+# import random
+# message = "welcome to code nation!"
+
+# # mylist = ["apple", "banana", "cherry"]
+# # x = len(mylist)
+# # print(x)
+
+# x = len(message)
+
+# print(x)
+
+
+
+
+
+
+# activity 2
+
+alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for x in alphabet:
+    print(x)
+
+answer=input ("please choose a number from 1 to 26......"   )
+
+if answer == alphabet:
+    print(alphabet)
+    ord(alphabet)
+
+    user_choice= None
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+if answer == user_choice:
+    print ord(alphabet)
+
+x = alphabet.index(user_choice)
+
+print(user_choice)
+
+
+
+
+
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# x = len(alphabet)
+# print(x)
+
+    
+# name = input("enter you name.....")
+# print(f"welcome to your doom {name} procede if you dare!")
+
+
+
+# activity 3
+
+
+# activity 4
+
+
+
+# activity 5
+
+# activity 6
---
+++ pythonrecap.py
@@ -0,0 +1,42 @@
+# music = "classical"
+
+# if music == "classical":
+#     print("oh no its clasical")
+
+# elif music == "pop":
+#     print("turn it up")
+
+# else:
+#     print("yay")
+
+# num1=10
+# num=20
+
+# if num1 > num2:
+#     print("number 1 is bigger")
+# elif num < num2:
+#     print("number 2 is bigger")
+# else:
+#     print(" both are th same")
+
+# num = 14
+
+# if num%3==0:
+#     print("fizz")
+
+# elif num%7==0:
+#     print("buzz")  
+
+# else:
+#     print("something")      
+
+
+# def light_switch():
+#     print("who turned out the lights")
+# light_switch()    
+
+# def cash_withdrawal (amount, accnum):
+#     print(f"withdrawing {amount} from account{accnum}")
+
+# cash_withdrawal(300, 504499219)
+
