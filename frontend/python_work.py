2aa7d6ae89aa4d6336884892ca7ebf7eee03867e
ash man <ashmankartel6@gmail.com>
2022-05-06 16:57:01 +0100

    5pm update

---
+++ frontend/python_work.py
@@ -0,0 +1,123 @@
+# casting
+# print(int(5.4))
+# print(int("54"))
+# print(float(54))
+# print(str(54))
+# print(str(5.4))
+# print(type(str(54)))
+# print(type(str(5.4)))
+
+
+# print("what is your name?")
+# name= input()
+
+# if name:
+#     print(f"hello {name}")
+
+# else:
+#     print("you did not provide name")
+
+
+# print(not True)
+# print(not False)
+
+# bool = False
+# if bool != True:
+#     print(False)
+# else:
+#     print(True)    
+
+
+
+# def add_up():
+#     num1 = input ("enter a number")
+#     num2 = input ("now enter another number")
+#     # print(int(num1) + int(num2))
+
+#     try:
+#         print(f"{num1} + {num2} is {int(num1) + int(num2)}")
+
+#     except:
+#         print("error!")
+#         print("*****************")
+#         add_up()    
+
+# add_up()
+
+
+# light = False
+
+# def light_switch():
+#     global light
+#     if light:
+#         print("its bright")
+#         light = False
+#     else:
+#         print("who turned out the light")
+#         light = True
+
+# light_switch()
+# light_switch()         
+
+# balance = 100
+# def cash_withdraw(amount):
+#     global balance
+#     print(f"your balance is  {balance}")
+#     print(f"you withdraw {amount}")
+#     balance -=amount
+#     print(f"new balance is {balance}")
+
+# cash_withdraw(20)    
+# cash_withdraw(20)  
+# cash_withdraw(20)  
+# cash_withdraw(20)  
+
+# even_nums = [2, 4, 6, 8, 10]
+
+# odd_nums = [1, 3, 5, 7, 9,]
+
+# even_nums.append(12)
+# even_nums.insert(0, 0)
+
+# for i in even_num:
+#     print(i)
+
+
+# even_nums = [2, 4, 6, 8, 10]
+
+# odd_nums = [1, 3, 5, 7, 9,]
+
+# even_nums.append(12)
+# even_nums.insert(0, 0)
+
+# odd_nums.remove
+# odd_nums.pop
+
+list_of_fruit =[
+    "apples",
+    "bananas",
+    "oranges",
+    "pears",
+    "grapes",
+    "pineapple",
+    "star-fruit",
+    "mango",
+    "cherries",
+    "black-berries"
+]
+# print(list_of_fruit[0:10])
+
+# print(list_of_fruit[0:10:2])
+
+# nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(nums[0:11:3])
+
+num = 0
+
+while num < 10:
+    print(num)
+    num +- 1
+
+
+loop_runs = True    
